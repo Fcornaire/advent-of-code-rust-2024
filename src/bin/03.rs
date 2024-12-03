63f6a21ada58563bedc3c4161c3da458a52c47a1
Cornaire Francis <cornairemax@yahoo.fr>
2024-12-03 20:21:59 +0100

    day 3 soluce finish

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,6 +1,5 @@
 use itertools::Itertools;
 use regex::Regex;
-use tracing::info;
 
 advent_of_code::solution!(3);
 
@@ -21,10 +20,7 @@ fn calculate_mul_results(instructions: Vec<(i32, i32)>) -> Vec<i32> {
 
 pub fn part_one(input: &str) -> Option<u32> {
     let valid_instructions = extract_valid_mul_instructions(input);
-    info!("valid_instructions: {:?}", valid_instructions);
-
     let results = calculate_mul_results(valid_instructions);
-    info!("results: {:?}", results);
 
     Some(results.iter().sum::<i32>() as u32)
 }
@@ -35,36 +31,56 @@ fn extract_valid_mul_instructions_with_do_dont(line: &str) -> Vec<(i32, i32)> {
     let re_dont = Regex::new(r"don't\(\)").unwrap();
 
     let mut instructions = Vec::new();
-    let dont_start_end_index:Vec<usize> = re_dont.captures_iter(line).map(|cap| cap.get(0).unwrap().start()).sorted() .collect();
+    let dont_start_indexes: Vec<usize> = re_dont
+        .captures_iter(line)
+        .map(|cap| cap.get(0).unwrap().start())
+        .sorted()
+        .collect();
 
-    let mut do_start_end_index:Vec<usize> = re_do.captures_iter(line).map(|cap| cap.get(0).unwrap().start()).sorted().collect();
-    do_start_end_index.insert(0, 0);
+    let mut do_start_indexes: Vec<usize> = re_do
+        .captures_iter(line)
+        .map(|cap| cap.get(0).unwrap().start())
+        .sorted()
+        .collect();
+    do_start_indexes.insert(0, 0);
+    do_start_indexes.push(line.len());
 
+    let unsafe_index: Vec<(usize, usize)> = dont_start_indexes
+        .iter()
+        .map(|dont| {
+            (
+                *dont,
+                *do_start_indexes
+                    .iter()
+                    .filter(|do_| *do_ > dont)
+                    .min()
+                    .unwrap_or(&0),
+            )
+        })
+        .collect();
 
-    re_mul.captures_iter(line).for_each((|cap| {
-        let first_dont_index_before_current = dont_start_end_index.iter().filter(|filt| filt > &&cap.get(0).unwrap().start()).min().unwrap_or(&0 );
-        let first_do_index_before_current = do_start_end_index.iter().filter(|filt| filt < &&cap.get(0).unwrap().start()).max().unwrap_or(&0 );
-
-        info!("first_do_index_before_current {:?} < {:?} < first_dont_index_before_current {:?}", first_do_index_before_current, cap.get(0).unwrap().start(), first_dont_index_before_current);
-
-        if *first_do_index_before_current < cap.get(0).unwrap().start() && cap.get(0).unwrap().start() > *first_dont_index_before_current   {
+    re_mul.captures_iter(line).for_each(|cap| {
+        if unsafe_index
+            .iter()
+            .filter(|(dont, _do)| {
+                cap.get(0).unwrap().start() > *dont && cap.get(0).unwrap().start() < *_do
+            })
+            .count()
+            == 0
+        {
             let x = cap[1].parse::<i32>().unwrap();
             let y = cap[2].parse::<i32>().unwrap();
 
             instructions.push((x, y));
         }
-
-    }));
+    });
 
     instructions
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     let valid_instructions = extract_valid_mul_instructions_with_do_dont(input);
-    info!("valid_instructions: {:?}", valid_instructions);
-
     let results = calculate_mul_results(valid_instructions);
-    info!("results: {:?}", results);
 
     Some(results.iter().sum::<i32>() as u32)
 }
